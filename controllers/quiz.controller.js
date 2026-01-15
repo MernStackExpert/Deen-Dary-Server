483a679ddcb7848a7c3c3f84f1bf4d0cc934252a
MernStackExpert <mdnirob30k@gmail.com>
2026-01-15 21:21:07 +0600

    Add and Complete Quiz Controller

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,16 +1,73 @@
-const { getDb } = require("../config/db");
+const { getDb, ObjectId } = require("../config/db");
 
 const connectDB = async () => {
   const db = await getDb();
   return db.collection("quiz");
 };
 
-// const getQuiz = async (params) => {
-//   try {
-//     const quizCollection = await connectDB();
+const getQuiz = async (req, res) => {
+  try {
+    const quizCollection = await connectDB();
 
-//   } catch (error) {}
-// };
+    const query = {};
+
+    // category
+    if (req.query.category) {
+      query.category = req.query.category;
+    }
+
+    // search
+    if (req.query.search) {
+      query.name = { $regex: req.query.search, $options: "i" };
+    }
+
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    // total quiz
+    const totalQuiz = await quizCollection.countDocuments(query);
+
+    const quizs = await quizCollection
+      .find(query)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+
+    res.send({
+      totalQuiz,
+      totalPages: Math.ceil(totalQuiz / limit),
+      quizs,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Failed to fetch Quiz", error });
+  }
+};
+
+const singleQuiz = async (req, res) => {
+  try {
+    const quizCollection = await connectDB();
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid Quiz id" });
+    }
+
+    const quiz = await quizCollection.findOne({ _id: new ObjectId(id) });
+
+    if (!quiz) {
+      return res.status(404).send({ message: "Quiz Not Found" });
+    }
+
+    res.send(quiz);
+  } catch (error) {
+    res.status(500).send({
+      message: "Failed to fetch product",
+      error,
+    });
+  }
+};
 
 const addQuiz = async (req, res) => {
   try {
@@ -22,6 +79,7 @@ const addQuiz = async (req, res) => {
       timeLimit: 30,
       points: 1,
       createdAt: new Date(),
+      updatedAt: new Date(),
     };
 
     const result = await quizCollection.insertOne(quiz);
@@ -36,3 +94,56 @@ const addQuiz = async (req, res) => {
     });
   }
 };
+
+const updateQuiz = async (req, res) => {
+  try {
+    const quizCollection = await connectDB();
+    const id = req.params.id;
+    const updateQuiz = {
+      ...req.body,
+      language: "bn",
+      timeLimit: 30,
+      points: 1,
+      updatedAt: new Date(),
+    };
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid Quiz id" });
+    }
+
+    const result = await quizCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updateQuiz }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).send({ message: "Quiz NOt Found" });
+    }
+    res.send({ message: "Quiz updated successfully", result });
+  } catch (error) {
+    res.status(500).send({ message: "Failed to update Quiz", error });
+  }
+};
+
+const deleteQuiz = async (req, res) => {
+  try {
+    const quizCollection = await connectDB();
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid Quiz id" });
+    }
+
+    const result = await quizCollection.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).send({ message: "Quiz Not Found" });
+    }
+
+    res.send({ message: "Quiz deleted successfully", result });
+  } catch (error) {
+    res.status(500).send({ message: "Failed to delete Quiz", error });
+  }
+};
+
+module.exports = { getQuiz, addQuiz, singleQuiz, updateQuiz, deleteQuiz };
